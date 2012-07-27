25d2512f278c7794bf6d37e55275c2019d0d8848
Chris YT Huang <ythuang@gmail.com>
2012-07-27 14:43:10 +0800

    deploy saltstack minions

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,8 +1,42 @@
 # deployment of salt-minions
 
+env.user = 'setusername'
+env.password = 'setpassword'
+env.hosts = []
+
 def deploy():
-  sudo('apt-get -y install python-software-properties')
-  sudo('add-apt-repository -y ppa:saltstack/salt')
-  sudo('apt-get update')
+    install_py_soft_properties()
+    add_saltstack_ppa()
+    update_repo()
+    install_salt_minion()
 
 
+def install_py_soft_properties():
+    '''
+        install python-software-properties software package
+    '''
+    sudo('apt-get -y install python-software-properties')
+
+def add_saltstack_ppa():
+    '''
+        add saltstack ppa
+    '''
+    sudo('add-apt-repository -y ppa:saltstack/salt')
+
+def update_repo():
+    '''
+        update the repository
+    '''
+    sudo('apt-get update')
+
+def install_salt_minion():
+    '''
+        install salt-minion package
+    '''
+    sudo('apt-get install salt-minion')
+
+def config_salt_minion():
+    '''
+        configure salt-minion
+    '''
+    
