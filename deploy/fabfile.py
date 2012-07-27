3faa7943ed6d8d3151acce439676ba77e3c2a223
Chris YT Huang <ythuang@gmail.com>
2012-07-27 15:07:37 +0800

    deploy master and minion

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,14 +1,26 @@
 # deployment of salt-minions
 
+from fabric.contrib import files
+
+
 env.user = 'setusername'
 env.password = 'setpassword'
 env.hosts = []
 
-def deploy():
+def deploy_pre():
     install_py_soft_properties()
     add_saltstack_ppa()
     update_repo()
+
+def deploy_salt_master():
+    deploy_pre()
+    install_salt_master()
+    config_salt_master()
+
+def deploy_salt_minion():
+    deploy_pre()
     install_salt_minion()
+    config_salt_minion()
 
 
 def install_py_soft_properties():
@@ -35,8 +47,20 @@ def install_salt_minion():
     '''
     sudo('apt-get install salt-minion')
 
+def install_salt_master():
+    '''
+        install salt-master package
+    '''
+    sudo('apt-get install salt-master')
+
 def config_salt_minion():
     '''
         configure salt-minion
     '''
-    
+    fabric.operations.put('minion','/etc/salt/minion',use_sudo=True,mode=0644)
+
+def config_salt_master():
+    '''
+        configure salt-master
+    '''
+    fabric.operations.put('master','/etc/salt/master',use_sudo=True,mode=0644)
